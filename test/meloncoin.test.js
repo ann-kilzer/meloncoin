b71886ef17fbe914517d7fdba12320a38666faff
Ann Kilzer <akilzer@gmail.com>
2018-08-12 22:18:45 +0900

    javascript tests coming along

--- test/meloncoin.test.js
+++ test/meloncoin.test.js
@@ -1,13 +1,18 @@
 var Meloncoin = artifacts.require("Meloncoin.sol");
 
 contract('Meloncoin', function(accounts) {
-    it("should launch a new meloncoin", function() {
-	var meloncoin;
-	return Meloncoin.new(3, 0, 90, 10, accounts[0]).then(function(instance) {
-	    meloncoin = instance;
-	    return meloncoin.totalSupply();
-	}).then(function(total) {
-	    assert.equal(total.valueOf(), 3000000000000000000, "Total supply wasn't 3*10^18");
-	});
+
+    beforeEach(async function () {
+	this.plantTime = Math.floor((new Date).getTime() / 1000); // seconds since the epoch
+	this.meloncoin = await Meloncoin.new(3, this.plantTime, 90, 10, accounts[0]);
+    });
+    
+    it("should launch a new meloncoin", async function() {
+	var total = await this.meloncoin.totalSupply();
+	assert.equal(total.valueOf(), 3000000000000000000, "Total supply wasn't 3*10^18");
+	var decimals = await this.meloncoin.decimals();
+	assert.equal(decimals.valueOf(), 18, "Decimals wasn't 18");
+	var plantTime = await this.meloncoin.plantDate();
+	assert.equal(plantTime, this.plantTime, "Plant date doesn't match what we recorded");
     });
 });
